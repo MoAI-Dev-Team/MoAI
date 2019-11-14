3b0f586f1f0e344065f09b4d87c3eb478d24c8e2
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-14 13:47:28 +0900

    Add reduction(uncompleted)

--- src/SKI.hs
+++ src/SKI.hs
@@ -3,3 +3,9 @@ module SKI
     ) where
     
 data SKITerm = SKITerm :$ SKITerm | S | K | I deriving (Show, Eq)
+
+reduction :: SKITerm -> SKITerm
+reduction (((S :$ x) :$ y) :$ z) = (x :$ z) :$ (y :$ z)
+reduction ((K :$ x) :$ y) = x
+reduction (I :$ x) = x
+reduction (function :$ argument) = (reduction function)
