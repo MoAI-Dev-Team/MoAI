f3daead612a27ee8351203fe853ae2fc63874683
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-14 17:58:17 +0900

    Update SKI.hs

--- src/SKI.hs
+++ src/SKI.hs
@@ -5,8 +5,8 @@ module SKI
     
 data SKITerm = SKITerm :$ SKITerm | S | K | I deriving (Show, Eq)
 
-reduction :: SKITerm -> Either SKITrem SKITerm
-reduction (((S :$ x) :$ y) :$ z) =  Left ((x :$ z) :$ (y :$ z))
-reduction ((K :$ x) :$ y) = Left x
-reduction (I :$ x) = Left x
+reduction :: SKITerm -> SKITerm
+reduction (((S :$ x) :$ y) :$ z) = ((x :$ z) :$ (y :$ z))
+reduction ((K :$ x) :$ y) = x
+reduction (I :$ x) = x
 reduction (function :$ argument) = (reduction function) :$ (reduction argument)
