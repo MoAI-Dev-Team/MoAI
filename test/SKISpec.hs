ad79853e99e941482e81c2deb1168cac930e9acc
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-15 16:07:18 +0900

    Add test of I combinator

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -2,3 +2,9 @@ module SKISpec () where
 
 import Test.Hspec
 import SKI
+
+spec :: Spec
+spec = describe "SKI.reduction" $ do
+  context "reduct (I I)" $ do
+    it "sould return I" $ do
+      reduct (I :$ I) `shouldBe` I
