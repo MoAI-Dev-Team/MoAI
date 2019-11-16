266fb02301ae07aafb9d15dd232ea51254b8d389
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-16 15:28:52 +0900

    Fix symbol

--- src/SKI.hs
+++ src/SKI.hs
@@ -6,7 +6,7 @@ module SKI
 data SKITerm = SKITerm :$ SKITerm | S | K | I deriving (Show, Eq)
 
 reduction :: SKITerm -> SKITerm
-reduction (((S :$ x) :$ y) :$ z) = ((x :$ z) :$ (y :$ z))
-reduction ((K :$ x) :$ y) = x
-reduction (I :$ x) = x
-reduction (function :$ argument) = (reduction function) :$ (reduction argument)
+reduction (((S $: x) $: y) $: z) = ((x $: z) $: (y $: z))
+reduction ((K $: x) $: y) = x
+reduction (I $: x) = x
+reduction (function $: argument) = (reduction function) $: (reduction argument)
