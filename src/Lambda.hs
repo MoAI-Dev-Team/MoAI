5d9f86fe906f47dc60125d5061e4f7181be2e951
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-16 15:53:38 +0900

    Add type

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,1 +1,9 @@
+module Lambda
+    (
+    ) where
 
+data LambdaTerm = LambdaApply LambdaTerm LambdaTerm | LambdaAbstract String LambdaTerm | LambdaVariable String
+
+lamapp = LambdaApply
+lamabs = LambdaAbstract
+lamvar = LambdaVariable
