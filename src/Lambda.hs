c68daf2708c19f41d9a17135f2ebdc0eb9f5229f
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-21 16:20:44 +0900

    Delete prefix

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -4,8 +4,4 @@ module Lambda
     , lamvar
     ) where
 
-data LambdaTerm = LambdaApply LambdaTerm LambdaTerm | LambdaAbstract String LambdaTerm | LambdaVariable String
-
-lamapp = LambdaApply
-lamabs = LambdaAbstract
-lamvar = LambdaVariable
+data LambdaTerm = Apply LambdaTerm LambdaTerm | Abstract String LambdaTerm | Variable String
