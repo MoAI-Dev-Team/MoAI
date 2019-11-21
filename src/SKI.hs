7b1e3e753732d66d32844ead5c99c95c756c8ed1
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-21 14:44:38 +0900

    Delete brackets

--- src/SKI.hs
+++ src/SKI.hs
@@ -6,7 +6,7 @@ module SKI
 data SKITerm = Apply SKITerm SKITerm | S | K | I deriving (Show, Eq)
 
 reduction :: SKITerm -> SKITerm
-reduction (((S `Apply` x) `Apply` y) `Apply` z) = ((x `Apply` z) `Apply` (y `Apply` z))
-reduction ((K `Apply` x) `Apply` _) = x
+reduction (S `Apply` x `Apply` y `Apply` z) = x `Apply` z `Apply` (y `Apply` z)
+reduction (K `Apply` x `Apply` _) = x
 reduction (I `Apply` x) = x
-reduction (function `Apply` argument) = (reduction function) `Apply` (reduction argument)
+reduction (function `Apply` argument) = reduction function `Apply` reduction argument
