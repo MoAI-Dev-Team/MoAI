b036b1278301dd1bf4c82254932467f994e73eb9
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-21 23:05:57 +0900

    Update Lambda.hs

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,14 +1,15 @@
 module Lambda
     ( LambdaTerm(..)
-    , reduct
+    , reduction
     ) where
 
-data LambdaTerm = Apply LambdaTerm LambdaTerm | Abstract String LambdaTerm | Variable String
+data LambdaTerm = Apply LambdaTerm LambdaTerm | Abstract String LambdaTerm | Variable String deriving (Show, Eq)
 
 replace :: String -> LambdaTerm -> LambdaTerm -> LambdaTerm
 replace s x (y `Apply` z) = replace s x y `Apply` replace s x z
-replace s1 x (Abstract s2 y) = Abstract s2 y
+replace s1 x (Abstract s2 y) = Abstract s2 $ replace s1 x y
+replace s1 x y@(Variable s2) = if s1 == s2 then x else y
 
-reduct :: LambdaTerm -> LambdaTerm
-reduct ((Abstract s x) `Apply` y) = replace s y x
-reduct = id
+reduction :: LambdaTerm -> LambdaTerm
+reduction ((Abstract s x) `Apply` y) = replace s y x
+reduction x = x
