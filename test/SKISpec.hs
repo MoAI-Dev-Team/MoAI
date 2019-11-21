be62dcb5f182a027c0d0b92757f40f7818cc37e4
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-21 16:15:29 +0900

    Fix operator

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -3,14 +3,11 @@ module SKISpec (spec) where
 import Test.Hspec
 import SKI
 
-test :: Int
-test = 1
-
 spec :: Spec
 spec = describe "SKI.reduction" $ do
   context "reduct I I" $ do
     it "sould return I" $ do
-      reduction (I $: I) `shouldBe` I
+      reduction (I `Apply` I) `shouldBe` I
   context "reduct K S I" $ do
     it "should return S" $ do
-      reduction ((K $: S) $: I) `shouldBe` S
+      reduction (K `Apply` S `Apply` I) `shouldBe` S
