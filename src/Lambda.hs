272806c279623d8c691de3d15ebb61abc0da837e
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-21 18:23:23 +0900

    Add replace and so on...

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -1,7 +1,14 @@
 module Lambda
-    ( lamapp
-    , lamabs
-    , lamvar
+    ( LambdaTerm(..)
+    , reduct
     ) where
 
 data LambdaTerm = Apply LambdaTerm LambdaTerm | Abstract String LambdaTerm | Variable String
+
+replace :: String -> LambdaTerm -> LambdaTerm -> LambdaTerm
+replace s x (y `Apply` z) = replace s x y `Apply` replace s x z
+replace s1 x (Abstract s2 y) = Abstract s2 y
+
+reduct :: LambdaTerm -> LambdaTerm
+reduct ((Abstract s x) `Apply` y) = replace s y x
+reduct = id
