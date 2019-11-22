bca50aa4f79fdadd0cd89f3ce338a679f4ade8bc
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-22 11:17:04 +0900

    Add a test

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -1,1 +1,10 @@
+module LambdaSpec (spec) where
 
+import Test.Hspec
+import Lambda
+
+spec :: Spec
+spec = describe "Lambda.reduction" $ do
+  context "reduct (λx.x) y" $ do
+    it "should return y" $ do
+      reduction $ Abstract "x" (Variable "x") `Apply` Variable "y"
