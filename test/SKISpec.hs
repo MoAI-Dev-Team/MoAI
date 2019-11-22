1b8ad514815bd7af0cab0c3d26c2643abd177f93
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-22 12:29:41 +0900

    Add space

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -8,9 +8,11 @@ spec = describe "SKI.reduction" $ do
   context "reduct I I" $ do
     it "sould return I" $ do
       reduction (I `Apply` I) `shouldBe` I
+      
   context "reduct K S I" $ do
     it "should return S" $ do
       reduction (K `Apply` S `Apply` I) `shouldBe` S
+      
   context "reduct S S K I" $ do 
     it "should return S I (K I)" $ do
       reduction (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
