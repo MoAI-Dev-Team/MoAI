b1c215e4ac8e7ec5b176093362e5fafc0c1224ec
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-11-22 12:11:27 +0900

    Fit test

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -7,4 +7,4 @@ spec :: Spec
 spec = describe "Lambda.reduction" $ do
   context "reduct (λx.x) y" $ do
     it "should return y" $ do
-      reduction $ Abstract "x" (Variable "x") `Apply` Variable "y"
+      (reduction $ Abstract "x" (Variable "x") `Apply` Variable "y") `shouldBe` Variable "y"
