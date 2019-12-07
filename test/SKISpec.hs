50b6e5b0fd0192e597fa6c843299b0fa340358ac
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-07 12:05:29 +0900

    Add Maybe

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -5,14 +5,18 @@ import SKI
 
 spec :: Spec
 spec = describe "SKI.reduction" $ do
+  context "reduct S" $ do
+    it "should return Nothing" $ do
+      reduction S `shouldBe` Nothing
+
   context "reduct I I" $ do
-    it "sould return I" $ do
-      reduction (I `Apply` I) `shouldBe` I
+    it "sould return Just I" $ do
+      reduction (I `Apply` I) `shouldBe` Just I
       
   context "reduct K S I" $ do
-    it "should return S" $ do
-      reduction (K `Apply` S `Apply` I) `shouldBe` S
+    it "should return Just S" $ do
+      reduction (K `Apply` S `Apply` I) `shouldBe` Just S
       
   context "reduct S S K I" $ do 
-    it "should return S I (K I)" $ do
-      reduction (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
+    it "should return Just S I (K I)" $ do
+      reduction (S `Apply` S `Apply` K `Apply` I) `shouldBe` Just (S `Apply` I `Apply` (K `Apply` I))
