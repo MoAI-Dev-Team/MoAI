bec07c1fa63d954777ac8b4ed28e112a8e924573
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-07 12:39:44 +0900

    Add test of reducting Apply

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -6,13 +6,21 @@ import SKI
 spec :: Spec
 spec = describe "SKI.reduction" $ do
   context "reduct I I" $ do
-    it "sould return Just I" $ do
+    it "sould return I" $ do
       reduction (I `Apply` I) `shouldBe` I
       
   context "reduct K S I" $ do
-    it "should return Just S" $ do
+    it "should return S" $ do
       reduction (K `Apply` S `Apply` I) `shouldBe` S
       
   context "reduct S S K I" $ do 
-    it "should return Just S I (K I)" $ do
+    it "should return S I (K I)" $ do
       reduction (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
+  
+  context "reduct S (I K)" $ do
+    it "should return S K" $ do
+      reduction (S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` K)
+  
+  context "reduct I S (I K)" $ do
+    it "should return S (I K)" $ do
+      reduction (I `Apply` S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` (I `Apply` K))
