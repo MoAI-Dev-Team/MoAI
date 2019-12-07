4a80f6c82fb6d5cad8a7d601eddb173a6fbe7096
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-07 14:19:13 +0900

    Add test of reducting Apply

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -5,6 +5,18 @@ import Lambda
 
 spec :: Spec
 spec = describe "Lambda.reduction" $ do
+  let x = Variable "x"
+      y = Variable "y"
+      xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
+  
   context "reduct (λx.x) y" $ do
     it "should return y" $ do
-      (reduction $ Abstract "x" (Variable "x") `Apply` Variable "y") `shouldBe` Variable "y"
+      reduction xxy `shouldBe` y
+  
+  context "reduct x ((λx.x) y)" $ do
+    it "should return x y" $ do
+      reduction (x `Apply` xxy) `shouldBe` (x `Apply` y)
+  
+  context "reduct (λx.x) y ((λx.x) y)" $ do
+    it "should return y ((λx.x) y)" $ do
+      reduction (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
