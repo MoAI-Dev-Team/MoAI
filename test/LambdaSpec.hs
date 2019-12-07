2564fc459f8558143dd0aee84ffa910ee7fab133
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-07 16:24:32 +0900

    Rename reduct to reduce

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -4,19 +4,19 @@ import Test.Hspec
 import Lambda
 
 spec :: Spec
-spec = describe "Lambda.reduction" $ do
+spec = describe "Lambda.reduce" $ do
   let x = Variable "x"
       y = Variable "y"
       xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
   
-  context "reduct (λx.x) y" $ do
+  context "reduce (λx.x) y" $ do
     it "should return y" $ do
-      reduction xxy `shouldBe` y
+      reduce xxy `shouldBe` y
   
-  context "reduct x ((λx.x) y)" $ do
+  context "reduce x ((λx.x) y)" $ do
     it "should return x y" $ do
-      reduction (x `Apply` xxy) `shouldBe` (x `Apply` y)
+      reduce (x `Apply` xxy) `shouldBe` (x `Apply` y)
   
-  context "reduct (λx.x) y ((λx.x) y)" $ do
+  context "reduce (λx.x) y ((λx.x) y)" $ do
     it "should return y ((λx.x) y)" $ do
-      reduction (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
+      reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
