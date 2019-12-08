9efc3b9c8209b61840ec32dbecb5bb4f3d651b0e
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-08 10:16:22 +0900

    Add instance

--- src/SKI.hs
+++ src/SKI.hs
@@ -6,8 +6,23 @@ module SKI
 import Data.Typeable
 import Data.Data
 
+import Types
+
 data SKITerm = Apply SKITerm SKITerm 
              | S 
              | K 
              | I 
              deriving (Typeable, Data, Show, Eq)
+
+instance Reducible SKITerm where
+    reduce (S `Apply` x `Apply` y `Apply` z) = x `Apply` z `Apply` (y `Apply` z)
+    reduce (K `Apply` x `Apply` _) = x
+    reduce (I `Apply` x) = x
+    reduce (f `Apply` x) = 
+        if freduced /= f
+        then freduced `Apply` x
+        else f `Apply` xreduced
+        where
+            freduced = reduce f
+            xreduced = reduce x
+    reduce x = x
