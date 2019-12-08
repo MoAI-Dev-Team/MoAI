4445e7a33f0f9c51745f29191208a2a9906a0f1d
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-08 10:13:34 +0900

    Remove Lambda instance

--- src/Types.hs
+++ src/Types.hs
@@ -2,7 +2,6 @@ module Types
     (
     ) where
 
-import Lambda
 import SKI
 
 class Eq a => Reducible a where
@@ -14,22 +13,6 @@ class Eq a => Reducible a where
         else reduceAll xreduced
         where
             xreduced = reduce x
-
-instance Reducible LambdaTerm where
-    reduce ((Abstract s x) `Apply` y) = replace s y x
-        where
-            replace :: String -> LambdaTerm -> LambdaTerm -> LambdaTerm
-            replace s x (y `Apply` z) = replace s x y `Apply` replace s x z
-            replace s1 x (Abstract s2 y) = Abstract s2 $ replace s1 x y
-            replace s1 x y@(Variable s2) = if s1 == s2 then x else y
-    reduce (f `Apply` x) = 
-        if freducted /= f
-        then freducted `Apply` x
-        else f `Apply` xreducted
-        where
-            freducted = reduce f
-            xreducted = reduce x
-            reduce x = x
 
 instance Reducible SKITerm where
     reduce (S `Apply` x `Apply` y `Apply` z) = x `Apply` z `Apply` (y `Apply` z)
