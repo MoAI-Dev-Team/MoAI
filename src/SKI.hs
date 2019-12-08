9635fead8e46c380af51bd9b9866834be0ff4bd8
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-08 09:48:02 +0900

    Remove methods

--- src/SKI.hs
+++ src/SKI.hs
@@ -7,17 +7,8 @@ module SKI
 import Data.Typeable
 import Data.Data
 
-data SKITerm = Apply SKITerm SKITerm | S | K | I deriving (Typeable, Data, Show, Eq)
-
-reduce :: SKITerm -> SKITerm
-reduce (S `Apply` x `Apply` y `Apply` z) = x `Apply` z `Apply` (y `Apply` z)
-reduce (K `Apply` x `Apply` _) = x
-reduce (I `Apply` x) = x
-reduce (f `Apply` x) = 
-  if freduced /= f
-  then freduced `Apply` x
-  else f `Apply` xreduced
-  where
-    freduced = reduce f
-    xreduced = reduce x
-reduce x = x
+data SKITerm = Apply SKITerm SKITerm 
+             | S 
+             | K 
+             | I 
+             deriving (Typeable, Data, Show, Eq)
