fc9a8632c74bfb890faa6d7aa693326891987ca1
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2019-12-08 19:03:42 +0900

    Add test for reduceAll

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -5,22 +5,26 @@ import SKI
 
 spec :: Spec
 spec = describe "SKI.reduce" $ do
-  context "reduce I I" $ do
-    it "sould return I" $ do
-      reduce (I `Apply` I) `shouldBe` I
+    context "reduce I I" $ do
+        it "sould return I" $ do
+            reduce (I `Apply` I) `shouldBe` I
       
-  context "reduce K S I" $ do
-    it "should return S" $ do
-      reduce (K `Apply` S `Apply` I) `shouldBe` S
+    context "reduce K S I" $ do
+        it "should return S" $ do
+            reduce (K `Apply` S `Apply` I) `shouldBe` S
       
-  context "reduce S S K I" $ do 
-    it "should return S I (K I)" $ do
-      reduce (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
+    context "reduce S S K I" $ do 
+        it "should return S I (K I)" $ do
+            reduce (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
   
-  context "reduce S (I K)" $ do
-    it "should return S K" $ do
-      reduce (S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` K)
+    context "reduce S (I K)" $ do
+        it "should return S K" $ do
+            reduce (S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` K)
   
-  context "reduce I S (I K)" $ do
-    it "should return S (I K)" $ do
-      reduce (I `Apply` S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` (I `Apply` K))
+    context "reduce I S (I K)" $ do
+        it "should return S (I K)" $ do
+            reduce (I `Apply` S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` (I `Apply` K))
+  
+    context "reduce I S (I K) to normal form" $ do
+        it "should return S K" $ do
+            reduceAll (I `Apply` S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` K)
