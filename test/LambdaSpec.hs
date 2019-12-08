42ff17321518a2e3241da034892cf6fc2b6557b1
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2019-12-08 19:04:48 +0900

    Fix indent

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -5,22 +5,22 @@ import Lambda
 
 spec :: Spec
 spec = describe "Lambda.reduce" $ do
-  let x = Variable "x"
-      y = Variable "y"
-      xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
+    let x = Variable "x"
+        y = Variable "y"
+        xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
   
-  context "reduce (λx.x) y" $ do
-    it "should return y" $ do
-      reduce xxy `shouldBe` y
+    context "reduce (λx.x) y" $ do
+        it "should return y" $ do
+            reduce xxy `shouldBe` y
   
-  context "reduce x ((λx.x) y)" $ do
-    it "should return x y" $ do
-      reduce (x `Apply` xxy) `shouldBe` (x `Apply` y)
+    context "reduce x ((λx.x) y)" $ do
+        it "should return x y" $ do
+            reduce (x `Apply` xxy) `shouldBe` (x `Apply` y)
   
-  context "reduce (λx.x) y ((λx.x) y)" $ do
-    it "should return y ((λx.x) y)" $ do
-      reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
+    context "reduce (λx.x) y ((λx.x) y)" $ do
+        it "should return y ((λx.x) y)" $ do
+            reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
       
-  context "reduce (λx.x) y ((λx.x) y) to normal form" $ do
-    it "should return y y" $ do
-      reduceAll (xxy `Apply` xxy) `shouldBe` (y `Apply` y)
+    context "reduce (λx.x) y ((λx.x) y) to normal form" $ do
+        it "should return y y" $ do
+            reduceAll (xxy `Apply` xxy) `shouldBe` (y `Apply` y)
