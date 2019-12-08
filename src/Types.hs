24293112aea11a839e337b10efb88ec773355ae0
MoAI-Dev-Term <57426965+MoAI-Dev-Term@users.noreply.github.com>
2019-12-08 10:15:37 +0900

    Remove SKI instance

--- src/Types.hs
+++ src/Types.hs
@@ -14,15 +14,3 @@ class Eq a => Reducible a where
         where
             xreduced = reduce x
 
-instance Reducible SKITerm where
-    reduce (S `Apply` x `Apply` y `Apply` z) = x `Apply` z `Apply` (y `Apply` z)
-    reduce (K `Apply` x `Apply` _) = x
-    reduce (I `Apply` x) = x
-    reduce (f `Apply` x) = 
-        if freduced /= f
-        then freduced `Apply` x
-        else f `Apply` xreduced
-        where
-            freduced = reduce f
-            xreduced = reduce x
-    reduce x = x
