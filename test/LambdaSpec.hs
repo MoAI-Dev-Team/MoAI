ed0ff552834b9f4058c449ec2221728e150b3fce
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2019-12-08 18:57:37 +0900

    Add test for reduceAll

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -20,3 +20,7 @@ spec = describe "Lambda.reduce" $ do
   context "reduce (λx.x) y ((λx.x) y)" $ do
     it "should return y ((λx.x) y)" $ do
       reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
+      
+  context "reduce (λx.x) y ((λx.x) y) to normal form" $ do
+    it "should return y y" $ do
+      reduceAll (xxy `Apply` xxy) `shouldBe` (y `Apply` y)
