bf78552399c25dcf1777f9ad44e189b2d6e9ae1d
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2019-12-09 09:19:01 +0900

    Update LambdaSpec.hs

--- test/LambdaSpec.hs
+++ test/LambdaSpec.hs
@@ -9,15 +9,15 @@ spec = describe "Lambda.reduce" $ do
         y = Variable "y"
         xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
   
-    context "reduce (λx.x) y" $ do
+    context "reduce (λx.x) y once" $ do
         it "should return y" $ do
             reduce xxy `shouldBe` y
   
-    context "reduce x ((λx.x) y)" $ do
+    context "reduce x ((λx.x) y) once" $ do
         it "should return x y" $ do
             reduce (x `Apply` xxy) `shouldBe` (x `Apply` y)
   
-    context "reduce (λx.x) y ((λx.x) y)" $ do
+    context "reduce (λx.x) y ((λx.x) y) once" $ do
         it "should return y ((λx.x) y)" $ do
             reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
       
