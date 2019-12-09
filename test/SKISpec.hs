e06c6aa1feb4fd7588bc6e8ac51f2ed32c7d60e6
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2019-12-09 09:19:58 +0900

    Update SKISpec.hs

--- test/SKISpec.hs
+++ test/SKISpec.hs
@@ -5,23 +5,23 @@ import SKI
 
 spec :: Spec
 spec = describe "SKI.reduce" $ do
-    context "reduce I I" $ do
+    context "reduce I I once" $ do
         it "sould return I" $ do
             reduce (I `Apply` I) `shouldBe` I
       
-    context "reduce K S I" $ do
+    context "reduce K S I once" $ do
         it "should return S" $ do
             reduce (K `Apply` S `Apply` I) `shouldBe` S
       
-    context "reduce S S K I" $ do 
+    context "reduce S S K I once" $ do 
         it "should return S I (K I)" $ do
             reduce (S `Apply` S `Apply` K `Apply` I) `shouldBe` (S `Apply` I `Apply` (K `Apply` I))
   
-    context "reduce S (I K)" $ do
+    context "reduce S (I K) once" $ do
         it "should return S K" $ do
             reduce (S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` K)
   
-    context "reduce I S (I K)" $ do
+    context "reduce I S (I K) once" $ do
         it "should return S (I K)" $ do
             reduce (I `Apply` S `Apply` (I `Apply` K)) `shouldBe` (S `Apply` (I `Apply` K))
   
