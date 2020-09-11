05d982592b5995efd40dc496b335df44836f322b
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2020-09-11 17:43:18 +0900

    Update LambdaIndex.hs

--- src/LambdaIndex.hs
+++ src/LambdaIndex.hs
@@ -11,14 +11,14 @@ import Types
 
 data LambdaTerm
     = Apply LambdaTerm LambdaTerm
-    | Abstract String LambdaTerm
-    | Variable String
+    | Abstract Int LambdaTerm
+    | Variable Int
     deriving (Typeable, Data, Show, Eq)
 
 instance Reducible LambdaTerm where
     reduce ((Abstract s x) `Apply` y) = replace s y x
         where
-            replace :: String -> LambdaTerm -> LambdaTerm -> LambdaTerm
+            replace :: Int -> LambdaTerm -> LambdaTerm -> LambdaTerm
             replace s x (y `Apply` z) = replace s x y `Apply` replace s x z
             replace s1 x (Abstract s2 y) = Abstract s2 $ replace s1 x y
             replace s1 x y@(Variable s2) = if s1 == s2 then x else y
