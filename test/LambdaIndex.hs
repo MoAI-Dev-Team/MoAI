90199d0e16f122667e197a5bf18ab3df351e4dae
MoAI-Dev-Team <57426965+MoAI-Dev-Team@users.noreply.github.com>
2020-09-11 17:45:23 +0900

    Create LambdaIndex.hs

---
+++ test/LambdaIndex.hs
@@ -0,0 +1,26 @@
+module LambdaSpec (spec) where
+
+import Test.Hspec
+import Lambda
+
+spec :: Spec
+spec = describe "Lambda.reduce" $ do
+    let x = Variable "x"
+        y = Variable "y"
+        xxy = Abstract "x" (Variable "x") `Apply` Variable "y"
+  
+    context "reduce (λx.x) y once" $ do
+        it "should return y" $ do
+            reduce xxy `shouldBe` y
+  
+    context "reduce x ((λx.x) y) once" $ do
+        it "should return x y" $ do
+            reduce (x `Apply` xxy) `shouldBe` (x `Apply` y)
+  
+    context "reduce (λx.x) y ((λx.x) y) once" $ do
+        it "should return y ((λx.x) y)" $ do
+            reduce (xxy `Apply` xxy) `shouldBe` (y `Apply` xxy)
+      
+    context "reduce (λx.x) y ((λx.x) y) to normal form" $ do
+        it "should return y y" $ do
+            reduceAll (xxy `Apply` xxy) `shouldBe` (y `Apply` y)
